bc0fd3eace08e90ee1d80c9ead77e05d746e05c3
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-01-05 03:20:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function driverNamesWithRevenueOver(drivers, revenue) {
 function exactMatch(drivers, obj) {
   return drivers.filter( function(driver){
     return driver === driver;
-  }); 
+  });
 }
